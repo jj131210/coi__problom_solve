e124c172e5ad1e231cb73fa3ccf716443e865c34
jj <164000317+jj131210@users.noreply.github.com>
2025-07-19 01:47:10 +0000

    1000 문제의 테스트 케이스 추가 및 28278 문제 파일 생성, C++ 코드 추가, README 수정

---
+++ 28278.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int rpt;
+    cin>>rpt;
+    for(int i=0;i<rtp;i++){
+        int first,second=-1;
+        cin>>first;
+        if(first==1) cin>>second;
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # coi__problom_solve
 
-this is c++ code solve
+this is c++ code solve                                  
