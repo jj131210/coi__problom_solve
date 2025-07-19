c8ac1e18f7083d37d3eefdad09b07b89995fd73f
jj <164000317+jj131210@users.noreply.github.com>
2025-07-19 01:48:50 +0000

    28278 문제의 C++ 코드 수정 및 테스트 케이스 업데이트

--- 28278.cpp
+++ 28278.cpp
@@ -3,12 +3,39 @@ using namespace std;
 
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    stack<int> st;
     int rpt;
     cin>>rpt;
-    for(int i=0;i<rtp;i++){
+    for(int i=0;i<rpt;i++){
         int first,second=-1;
         cin>>first;
-        if(first==1) cin>>second;
+        if(first==1){
+            cin>>second;
+            st.push(second);
+        }
+        else if(first==2){
+            if(st.size() == 0) cout<<-1<<'\n';
+            else{
+                cout<<st.top()<<'\n';
+                st.pop();
+            }
+        }
+        else if(first==3){
+            cout<<st.size()<<'\n';
+        }
+        else if(first==4){
+            if(st.empty())cout<<1<<'\n';
+            else cout<<0<<'\n';
+        }
+        else if(first==5){
+            if(st.size() == 0) cout<<-1<<'\n';
+            else{
+                cout<<st.top()<<'\n';
+            }
+        }
     }
     return 0;
 }
