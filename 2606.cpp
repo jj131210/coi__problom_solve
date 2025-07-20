aefdd75d8d5323dc8105dcb4c431e9d6f06e6a60
jj <164000317+jj131210@users.noreply.github.com>
2025-07-20 07:50:17 +0000

    Implement code changes to enhance functionality and improve performance

--- 2606.cpp
+++ 2606.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int n,m
-
+vector<bool> visited(n+1, false);
+vector<vector<int>> graph(n+1);
 int main(){
+    int n, m,;
     cin >> n >> m; 
-    vector<vector<int>> graph(n+1);
+    graph.resize(n + 1);
+    visited.resize(n + 1, false);
     for(int i=0;i<m;i++){
-        int a, b;
-        cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        int u, b;
+        cin >> u >> b;
+        graph[u].push_back(b);
+        graph[b].push_back(u);
     }
-    vector<bool> visited(n+1, false);
+    
     queue<int> q;
     q.push(1);
     visited[1] = true;
-    while (!q.empty()) {
-        intr v = q.front();
-        q.pop();
-        for(auto c :graph[v]) {
-            if (!visited[c]) {
-                visited[c] = true;
-                q.push(c);
-            }
-        }
-    {
-        /* code */
-    }
-    
+    // while (!q.empty()) {
+    //     int v = q.front();
+    //     q.pop();
+    //     for(auto c :graph[v]) {
+    //         if (visited[c] == false) {
+    //             q.push(c);
+    //             visited[c] = true;
+    //             infectedCount++;
+    //         }
+    //     }
+    // }
+    cout << infectedCount;
 }
 
 
